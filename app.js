975331b76d237039f03d0beba42d4605211e4278
mahendra24reddy <tammamahendrareddy24@gmail.com>
2023-08-07 14:16:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// special countdown 
+
+let dayItem=document.querySelector("#days");
+
+let hoursItem=document.querySelector("#hours");
+
+let minItem=document.querySelector("#min");
+
+let secItem=document.querySelector("#sec");
+
+
+let countDown=()=>{
+    let futureDate=new Date("15 aug 2023");
+    let currentDate=new Date();
+    let myDate=futureDate - currentDate;
+
+    let days=Math.floor(myDate / 1000 / 60 / 60 / 24 );
+    let hours=Math.floor(myDate / 1000 / 60  / 60) % 24;
+    let min=Math.floor(myDate / 1000 / 60)% 60;
+    let sec=Math.floor(myDate / 1000)% 60;
+
+
+    dayItem.innerHTML=days;
+    hoursItem.innerHTML=hours;
+    minItem.innerHTML=min;
+    secItem.innerHTML=sec;
+
+}
+
+countDown();
+
+setInterval(countDown,1000);
+
+
+// nav hide 
+
+
+let navBar=document.querySelectorAll('.nav-link');
+let navCollapse=document.querySelector('.navbar-collapse.collapse')
+
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
